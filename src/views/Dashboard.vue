41983f40e218ac849d60fdfee48f68a1f9ab0603
Julien Veyssier <eneiluj@posteo.net>
2020-09-19 01:56:36 +0200

    improve empty content
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,20 +4,18 @@
 		:show-more-text="title"
 		:loading="state === 'loading'">
 		<template v-slot:empty-content>
-			<div v-if="state === 'no-token'">
-				<a :href="settingsUrl">
-					{{ t('integration_reddit', 'Click here to configure the access to your Reddit account.') }}
-				</a>
-			</div>
-			<div v-else-if="state === 'error'">
-				<a :href="settingsUrl">
-					{{ t('integration_reddit', 'Incorrect access token.') }}
-					{{ t('integration_reddit', 'Click here to configure the access to your Reddit account.') }}
-				</a>
-			</div>
-			<div v-else-if="state === 'ok'">
-				{{ t('integration_reddit', 'Nothing to show') }}
-			</div>
+			<EmptyContent
+				v-if="emptyContentMessage"
+				:icon="emptyContentIcon">
+				<template #desc>
+					{{ emptyContentMessage }}
+					<div v-if="state === 'no-token' || state === 'error'" class="connect-button">
+						<a class="button" :href="settingsUrl">
+							{{ t('integration_reddit', 'Connect to Reddit') }}
+						</a>
+					</div>
+				</template>
+			</EmptyContent>
 		</template>
 	</DashboardWidget>
 </template>
@@ -26,16 +24,16 @@
 import axios from '@nextcloud/axios'
 import { generateUrl } from '@nextcloud/router'
 import { showError } from '@nextcloud/dialogs'
-// eslint-disable-next-line
 import { getLocale } from '@nextcloud/l10n'
 import moment from '@nextcloud/moment'
 import { DashboardWidget } from '@nextcloud/vue-dashboard'
+import EmptyContent from '@nextcloud/vue/dist/Components/EmptyContent'
 
 export default {
 	name: 'Dashboard',
 
 	components: {
-		DashboardWidget,
+		DashboardWidget, EmptyContent,
 	},
 
 	props: {
@@ -78,6 +76,26 @@ export default {
 		lastId() {
 			const nbNotif = this.notifications.length
 			return (nbNotif > 0) ? this.notifications[0].name : null
+		},
+		emptyContentMessage() {
+			if (this.state === 'no-token') {
+				return t('integration_reddit', 'No Reddit account connected')
+			} else if (this.state === 'error') {
+				return t('integration_reddit', 'Error connecting to Reddit')
+			} else if (this.state === 'ok') {
+				return t('integration_reddit', 'No Reddit notifications!')
+			}
+			return ''
+		},
+		emptyContentIcon() {
+			if (this.state === 'no-token') {
+				return 'icon-reddit'
+			} else if (this.state === 'error') {
+				return 'icon-close'
+			} else if (this.state === 'ok') {
+				return 'icon-checkmark'
+			}
+			return 'icon-checkmark'
 		},
 	},
 
@@ -170,4 +188,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
+::v-deep .connect-button {
+	margin-top: 10px;
+}
 </style>
