27893f57ed9be8437d3ec6668da65ccf0d814634
Julien Veyssier <eneiluj@posteo.net>
2020-12-10 14:49:07 +0100

    avoid crash when accessibility app is not installed
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -54,7 +54,6 @@ export default {
 			loop: null,
 			state: 'loading',
 			settingsUrl: generateUrl('/settings/user/connected-accounts'),
-			darkThemeColor: OCA.Accessibility.theme === 'dark' ? '181818' : 'ffffff',
 			themingColor: OCA.Theming ? OCA.Theming.color.replace('#', '') : '0082C9',
 		}
 	},
