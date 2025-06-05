ee36ab00fddb80439e593bac64dc15d8c6d86d08
Oleksander Piskun <oleksandr2088@icloud.com>
2025-06-05 13:53:45 +0300

    chore(maintenance): use of getCSPNonce; drop NC28, NC29
    Signed-off-by: Oleksander Piskun <oleksandr2088@icloud.com>

--- src/dashboard.js
+++ src/dashboard.js
@@ -3,15 +3,20 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-__webpack_nonce__ = btoa(OC.requestToken) // eslint-disable-line
-__webpack_public_path__ = OC.linkTo('integration_reddit', 'js/') // eslint-disable-line
+import { linkTo } from '@nextcloud/router'
+import { getCSPNonce } from '@nextcloud/auth'
 
-OCA.Dashboard.register('reddit_news', async (el, { widget }) => {
-	const { default: Vue } = await import(/* webpackChunkName: "dashboard-lazy" */'vue')
-	const { default: Dashboard } = await import(/* webpackChunkName: "dashboard-lazy" */'./views/Dashboard.vue')
-	Vue.mixin({ methods: { t, n } })
-	const View = Vue.extend(Dashboard)
-	new View({
-		propsData: { title: widget.title },
-	}).$mount(el)
+__webpack_nonce__ = getCSPNonce() // eslint-disable-line
+__webpack_public_path__ = linkTo('integration_reddit', 'js/') // eslint-disable-line
+
+document.addEventListener('DOMContentLoaded', () => {
+	OCA.Dashboard.register('reddit_news', async (el, { widget }) => {
+		const { default: Vue } = await import(/* webpackChunkName: "dashboard-lazy" */'vue')
+		const { default: Dashboard } = await import(/* webpackChunkName: "dashboard-lazy" */'./views/Dashboard.vue')
+		Vue.mixin({ methods: { t, n } })
+		const View = Vue.extend(Dashboard)
+		new View({
+			propsData: { title: widget.title },
+		}).$mount(el)
+	})
 })
